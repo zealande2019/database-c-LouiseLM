9e8022e7591b3ebf287a77e774a938fe221cffad
Louise Lytken Petersen Møller <loui142j@edu.easj.dk>
2019-02-07 13:01:34 +0100

    Start

---
+++ ConsoleAppDatabase/Facade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDatabase
+{
+     public static class Facade
+    {
+        public static List<Students> GetAllStudents()
+        {
+            List<Students> result = new List<Students>();
+
+            return result;
+        }
+
+        string SelectAllStudents = "select * from student";
+        string conn =
+
+    }
+}
---
+++ ConsoleAppDatabase/Students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDatabase
+{
+    class Students
+    {
+        public int StudentId { get; set; }
+        public int Name { get; set; }
+        public int MobilNr { get; set; }
+
+        public Students()
+        {
+
+        }
+    }
+}
